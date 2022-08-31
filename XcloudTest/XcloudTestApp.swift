093d11ad806ee22a7ef44324308d3e20107e4257
Nermeen Tomoum <nrmeen.tomoum@alumni.fci-cu.edu.eg>
2022-08-31 12:18:50 +0400

    Initial Commit

---
+++ XcloudTest/XcloudTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcloudTestApp.swift
+//  XcloudTest
+//
+//  Created by Nermeen Tomoum on 31/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct XcloudTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
